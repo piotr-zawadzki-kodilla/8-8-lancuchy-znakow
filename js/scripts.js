638e55e96667e6fe54fac592aa9f811376a06c6b
zaramid <piotr@zawadzki.net>
2017-03-12 15:47:55 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var text = "Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.";
+var animal = "Zielone słonie";
+
+animal = animal.toUpperCase();
+
+text = text.replace("Papugi", animal);
+
+console.log(text.substr(0, text.length/2));
